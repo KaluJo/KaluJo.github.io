9745399182845be2c98106fbffa4d43d63b21aba
Bhada Yun <bhadayun@gmail.com>
2023-11-28 12:19:12 -0800

    coding portfolio update

--- src/pages/Coding.js
+++ src/pages/Coding.js
@@ -43,17 +43,24 @@ const Coding = () => {
                 name={"Timeless Treasures"}
                 link={"https://timelesstreasures.app/"}
                 description={"I worked with a team on a full-stack design & development project.\n\nWe brought to life Timeless Treasures, a safe and personalized platform to share life stories with loved ones.\n\nThis project involved storyboarding, various brainstorming techniques, contextual inquiry, application of HCI principles, usability evaluation, accessibility heuristics, and task & competitive analysis in regards to building and maintaining a product."}
-                techStack={"React, Figma, Firebase, Styled Components, Chakra UI, Kommunicate"}
+                techStack={"React.js, Figma, Firebase, Styled Components, Chakra UI, Kommunicate"}
                 videoLink={"https://www.youtube.com/watch?v=qG-d7VOkXz8"}
             />
 
+            <ProjectSpotlight
+                name={"Cliniprompt / PromptQ"}
+                // link={"https://timelesstreasures.app/"}
+                description={"I worked with a team based in UC Berkeley and researchers from University of Wisconsin-Madison on a full-stack design & development project for a real-world LLM application.\n\nWe created PromptQ, an open-source framework that aids in prompt crowdsourcing and evaluation of various enterprise and open-source models. Our specific use-case was for a clinical setting, using our framework to help hospitals gather useful prompts related to patient care.\n\nTo mediate the \'technical jargon\' associated with AI/prompting we drew inspiration from Tinder and Duolingo for our design choices, implementing a swiping system as well as a leaderboard/streak system to encourage consistent participation through gamification."}
+                techStack={"React.js, Firebase, HuggingFace, Python, Flask, OpenAI API, LLaMA, Styled Components, Mantine"}
+                videoLink={"https://youtu.be/1TWhKbf55yc"}
+            />
 
             <ProjectSpotlight
                 name={"하ハ哈ha"}
                 link={"https://github.com/KaluJo/gagagaga"}
                 description={"There is no one size fits all method of learning.\n\nGiven someone's unique background, there is an equally unique way to design a learning strategy that works with their theory of mind and intuition about the world.\n\nAs for me, I grew up in a bilingual environment where I spoke and learned Korean & English. Later in 6th grade when I started learning Japanese, I realized that neither the Korean textbook '민나노 日本語' or the curriculum for English speakers \'felt right\' for me.\n\nFor some context, while Japanese and Korean are in different language families, they have remarkably similar grammar structure to an almost 1-to-1 translational level. Due to the Chinese language influence, the lexicons are similar as well.\n\nGiven these facts, it makes more sense for me to learn Japanese in Korean rather than English. However, while my grasp of Korean grammar and speaking is high, my reading skills and vocabulary comprehension are not as good as I haven't read many Korean books (yet).\n\nDue to this, when learning to read Japanese and new vocabulary, I would have an easier time memorizing through a mapping from English to Japanese. Eventually, during my self-learning journey, I developed a unique system for myself.\n\nFor grammar, I would learn about them on the Korean internet (with a VPN). I found that this helped me understand grammatical concepts in a more intuitive way (since Korean and Japanese are very grammatically similar).\n\nFor vocab drilling, however, I found it more effective to memorize using English. An example of this is since I normally think in English, I naturally come up with vocabulary much easier in English than in Korean.\n\nFor vocab in context, I would listen to the Japanese version of K-pop songs from my childhood and compare the lyrics. I also re-watched shows like Doraemon and Crayon Shin-Chan, shows I used to watch on TV when I lived in Korea.\n\nSide Tangent: While learning Mandarin in high school, I had a fun realization that I can learn to understand the pure \'meaning\' associated with an individual 汉子/漢字/한자/character, and translate things from \'meaning-to-language\' when trying to read these characters in Chinese, Japanese, or older Korean scriptures.\n\nIt's difficult to express how this truly feels using text, but I suppose words always come at a loss of information.\n\nAnyways, I created this project to mediate all of this language learning chaos."}
-                techStack={"React (TypeScript), Node.js, Netlify, MongoDB, DeepL API, GPT-4 API & Whisper API, Chakra"}
-                videoLink={"https://youtu.be/rWaQ0NSSshc"}
+                techStack={"React.js (TypeScript), Node.js, Netlify, MongoDB, DeepL API, GPT-4 API & Whisper API, Chakra"}
+                videoLink={"https://youtu.be/GWScHTS3SSw"}
                 size={300}
             />
 
@@ -61,8 +68,8 @@ const Coding = () => {
                 name={"Dootch"}
                 link={"https://github.com/KaluJo/gagagaga"}
                 description={"This is an active self-learning experiment.\n\nI wanted to try learning languages by only memorizing sentences. Without looking up grammar guides or textbooks, I created a random list of German vocabulary, and let GPT-4 generate a sentence for each word for me to memorize.\n\nI am hoping that this will help in two main ways:\n\n1) I hope it improves my memorization of genders by training myself to adopt the \'native/first language\' usage of gender:\n\n\"It just sounded right, that\'s just how it came out of my mouth.\"\n\nEssentially, I want to be able to just utter the correct word without having to place any conscious energy into it.\n\n2) I want to use it as an exercise to improve my pattern-matching and problem solving skills. It definitely does help that I already speak English, a Germanic language."}
-                techStack={"React (TypeScript), Node.js, Netlify, MongoDB, DeepL API, GPT-4 API, Chakra"}
-                videoLink={"https://youtu.be/X7T84kOzuBA"}
+                techStack={"React.js (TypeScript), Node.js, Netlify, MongoDB, DeepL API, GPT-4 API, Chakra"}
+                videoLink={"https://youtu.be/BJ1WUBI68bs"}
                 size={200}
             />
 
